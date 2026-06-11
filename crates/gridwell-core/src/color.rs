600bf629cb23c1e7545212e3468dba6f4282315d
Richard Iannone <riannone@me.com>
2026-06-10 21:15:50 -0400

    Add Color constructors and formatting helpers

--- crates/gridwell-core/src/color.rs
+++ crates/gridwell-core/src/color.rs
@@ -16,3 +16,39 @@ pub struct Color {
     pub a: u8,
 }
 
+impl Color {
+    pub const fn new(r: u8, g: u8, b: u8, a: u8) -> Self {
+        Self { r, g, b, a }
+    }
+
+    pub const fn rgb(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b, a: 255 }
+    }
+
+    pub const TRANSPARENT: Self = Self {
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 0,
+    };
+
+    /// To hex string (#RRGGBB or #RRGGBBAA if alpha < 255).
+    pub fn to_hex(&self) -> String {
+        if self.a == 255 {
+            format!("#{:02X}{:02X}{:02X}", self.r, self.g, self.b)
+        } else {
+            format!("#{:02X}{:02X}{:02X}{:02X}", self.r, self.g, self.b, self.a)
+        }
+    }
+
+    /// To rgb()/rgba() CSS functional notation.
+    pub fn to_css_rgb(&self) -> String {
+        if self.a == 255 {
+            format!("rgb({}, {}, {})", self.r, self.g, self.b)
+        } else {
+            let alpha = self.a as f64 / 255.0;
+            format!("rgba({}, {}, {}, {:.3})", self.r, self.g, self.b, alpha)
+        }
+    }
+}
+
