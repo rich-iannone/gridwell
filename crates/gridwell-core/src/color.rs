1f8cfede27862ba5328c6b116ec259bc45c608dd
Richard Iannone <riannone@me.com>
2026-06-10 21:17:26 -0400

    Add named_color lookup for common color names

--- crates/gridwell-core/src/color.rs
+++ crates/gridwell-core/src/color.rs
@@ -153,3 +153,26 @@ fn parse_rgb_func(inner: &str) -> Option<Color> {
     }
 }
 
+fn named_color(name: &str) -> Option<Color> {
+    let c = match name.to_ascii_lowercase().as_str() {
+        "black" => Color::rgb(0, 0, 0),
+        "white" => Color::rgb(255, 255, 255),
+        "red" => Color::rgb(255, 0, 0),
+        "green" => Color::rgb(0, 128, 0),
+        "blue" => Color::rgb(0, 0, 255),
+        "yellow" => Color::rgb(255, 255, 0),
+        "cyan" | "aqua" => Color::rgb(0, 255, 255),
+        "magenta" | "fuchsia" => Color::rgb(255, 0, 255),
+        "gray" | "grey" => Color::rgb(128, 128, 128),
+        "silver" => Color::rgb(192, 192, 192),
+        "maroon" => Color::rgb(128, 0, 0),
+        "olive" => Color::rgb(128, 128, 0),
+        "navy" => Color::rgb(0, 0, 128),
+        "teal" => Color::rgb(0, 128, 128),
+        "purple" => Color::rgb(128, 0, 128),
+        "orange" => Color::rgb(255, 165, 0),
+        _ => return None,
+    };
+    Some(c)
+}
+
